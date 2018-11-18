90106ff424f48e8104c65045e88649bbf49ac366
rayees <shaikhrayeesahmed@gmail.com>
2018-11-18 18:05:46 +0530

    Add store vuex, add wonders card.

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,13 @@
     <AppToolBar/>
     <v-content>
       <app-search-bar/>
+      <v-layout row wrap>
+
+        <v-flex xs12 sm12 md4 pa-3>
+          <AppWonderCard :wonders="wonders[0]"/>
+        </v-flex>
+
+      </v-layout>
     </v-content>
   </v-app>
 </template>
@@ -11,12 +18,23 @@
 <script>
     import AppToolBar from './components/ToolBar'
     import AppSearchBar from './components/SearchBar'
+    import AppWonderCard from './components/WonderCard'
 
     export default {
-      components: {
-          AppToolBar,
-          AppSearchBar
-      }
-
-    }
+    components: {
+        AppToolBar,
+        AppSearchBar,
+        AppWonderCard
+    },
+    data: () => ({
+        wonders: [{
+            "id": "WONDERS_1",
+            "place": "Taj Mahal",
+            "description": "The Taj Mahal meaning \"Crown of the Palace\") is an ivory-white marble mausoleum on the south bank of the Yamuna river in the Indian city of Agra. It was commissioned in 1632 by the Mughal emperor, Shah Jahan (reigned from 1628 to 1658), to house the tomb of his favourite wife, Mumtaz Mahal. The tomb is the centrepiece of a 17-hectare (42-acre) complex, which includes a mosque and a guest house, and is set in formal gardens bounded on three sides by a crenellated wall.",
+            "imageURL": "https://travel.home.sndimg.com/content/dam/images/travel/fullset/2015/10/12/new-seven-wonders-taj-mahal.jpg.rend.hgtvcom.616.462.suffix/1491581548979.jpeg",
+            "ratings": 3.5,
+            "likes": 4
+        }]
+    })
+  }
 </script>
---
+++ src/components/WonderCard.vue
@@ -0,0 +1,44 @@
+<template>
+    <v-hover>
+        <v-card hover slot-scope="{hover}">
+            <v-img :src="wonders.imageURL">
+                <v-container fill-height fluid pa-3 v-if="hover">
+                    <v-layout row wrap pa-0>
+                        <v-flex xs12 sm12 md12>
+                            <div style="background-color: #FAF6F4; opacity: 0.7;
+                       height: 100%; width: 100%">
+                                <p class="text-xs-right pa-1">
+                                    <v-icon size="20" color="black">favorite</v-icon>
+                                    <span style="font-size: 20px" class="ml-1 mr-2 green--text">{{wonders.likes}}</span>
+                                </p>
+                                <div class="text-xs-center">
+                                    <h3>{{wonders.place}}</h3>
+                                    <p>{{wonders.description}}</p>
+                                </div>
+                            </div>
+                        </v-flex>
+                    </v-layout>
+                </v-container>
+            </v-img>
+            <v-card-actions>
+                Ratings:
+                <v-rating v-model="wonders.ratings" color="#DD2926" background-color="#8C959D"
+                          empty-icon="star_border" full-icon="star_border" half-icon="star_border"
+                          half-increments hover></v-rating>
+                <v-spacer></v-spacer>
+                <h4>{{wonders.place}}</h4>
+            </v-card-actions>
+        </v-card>
+    </v-hover>
+</template>
+
+<script>
+    export default {
+        name: "WonderCard",
+        props: ["wonders"]
+    }
+</script>
+
+<style scoped>
+
+</style>
